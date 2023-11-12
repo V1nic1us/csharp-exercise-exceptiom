aa92da1e3578147db1498feea331822ee853f33c
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-12 14:43:05 -0400

    feat: add application exception for error when withdrawing

---
+++ csharp-exercise-exceptiom/Entities/ExceptionsAccount/DomainException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_exceptiom.Entities.Exceptions
+{
+    internal class DomainException: ApplicationException
+    {
+        public string Message { get; private set; }
+
+        public DomainException(string message)
+        {
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
